2c33d96f01d76f5af98d353d2409ef80291bcd14
Dror Levin <spatz@psybear.com>
2019-08-23 22:22:03 +0300

    Convert to MacAddress from &str instead of String, improve tests

--- src/mac_address.rs
+++ src/mac_address.rs
@@ -3,7 +3,7 @@ use std::convert::TryFrom;
 use std::fmt;
 
 #[derive(Clone, PartialEq, Eq, Hash, Deserialize)]
-#[serde(try_from = "String")]
+#[serde(try_from = "&str")]
 pub struct MacAddress([u8; 6]);
 
 impl MacAddress {
@@ -12,10 +12,10 @@ impl MacAddress {
     }
 }
 
-impl TryFrom<String> for MacAddress {
+impl TryFrom<&str> for MacAddress {
     type Error = crate::error::Error;
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
         let mut nums = value.split(':').map(|n| u8::from_str_radix(n, 16));
         let mut mac_addresses = [0u8; 6];
 
@@ -23,12 +23,16 @@ impl TryFrom<String> for MacAddress {
             *octet = if let Some(Ok(n)) = nums.next() {
                 n
             } else {
-                return Err(Self::Error::InvalidMacAddress { value });
+                return Err(Self::Error::InvalidMacAddress {
+                    value: value.to_string(),
+                });
             }
         }
 
         if nums.next().is_some() {
-            return Err(Self::Error::InvalidMacAddress { value });
+            return Err(Self::Error::InvalidMacAddress {
+                value: value.to_string(),
+            });
         }
 
         Ok(MacAddress(mac_addresses))
@@ -61,10 +65,34 @@ impl fmt::Debug for MacAddress {
 mod tests {
     use super::*;
 
+    macro_rules! bad {
+        ($value:expr) => {
+            match MacAddress::try_from($value) {
+                Ok(mac) => panic!("parsed {} to: {:#?}", $value, mac),
+                Err(crate::error::Error::InvalidMacAddress { .. }) => (),
+                Err(e) => panic!("failed with: {}", e),
+            }
+        };
+    }
+
+    fn good(value: &str) {
+        MacAddress::try_from(value).unwrap();
+    }
+
+    #[test]
+    fn test_try_from() {
+        bad!("wat");
+        good("00:01:02:03:04:05");
+        good("00:01:02:03:04:0");
+        bad!("00:01:02:03:04:");
+        bad!("00:01:02:03:04");
+        bad!(":00:01:02:03:04");
+    }
+
     #[test]
     fn test_display() {
-        let mac_string = "00:01:02:03:04:05".to_string();
-        let mac = MacAddress::try_from(mac_string.clone()).unwrap();
+        let mac_string = "00:01:02:03:04:05";
+        let mac = MacAddress::try_from(mac_string).unwrap();
         assert_eq!(format!("{}", mac), mac_string);
     }
 }
