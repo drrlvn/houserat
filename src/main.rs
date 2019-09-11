3f6ff0e10faf69c6a791621d5399b4f0b522df7d
Dror Levin <spatz@psybear.com>
2019-09-11 23:13:54 +0300

    Detect online devices on startup by resolving device hostname and sending keepalive to IP (fix #16)
    Also removed strum dependency to slightly reduce bloat.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use c_ares_resolver::Resolver;
 use config::NetworkAddresses;
-use crossbeam_channel::select;
+use crossbeam_channel::{never, select};
 use metadata::Metadata;
 use network::Event;
 use pnet::util::MacAddr;
-use std::collections::HashMap;
+use std::collections::{hash_map, HashMap};
 use std::path::PathBuf;
 use structopt::StructOpt;
-use strum_macros::Display;
 
 mod config;
 mod error;
@@ -26,11 +26,19 @@ struct Opt {
 
 type Result<T, E = error::Error> = std::result::Result<T, E>;
 
-#[derive(Debug, Display)]
-#[strum(serialize_all = "kebab_case")]
+#[derive(Debug)]
 enum Status {
     Arrived,
     Left,
+}
+
+impl std::fmt::Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::Arrived => write!(f, "arrived"),
+            Self::Left => write!(f, "left"),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -40,11 +48,13 @@ struct Tracking {
 }
 
 struct HouseRat {
+    interface_name: String,
     network_addresses: NetworkAddresses,
     socket: network::Socket,
     client: telegram::Client,
     cooldown: Option<chrono::Duration>,
     quiet_period: Option<config::Period>,
+    devices: Option<Vec<config::Device>>,
     rules: HashMap<MacAddr, Metadata>,
     online: HashMap<MacAddr, Tracking>,
 }
@@ -52,19 +62,26 @@ struct HouseRat {
 impl HouseRat {
     fn new(config: config::Config) -> Result<Self> {
         Ok(Self {
+            interface_name: config.interface.name,
             network_addresses: config.interface.addresses,
             socket: network::Socket::new(config.interface.index)?,
             client: telegram::Client::new(&config.bot_token),
             cooldown: config.cooldown,
             quiet_period: config.quiet_period,
+            devices: Some(config.devices),
             rules: config.rules,
             online: HashMap::new(),
         })
     }
 
-    fn run(&mut self, mut capture: pcap::Capture<pcap::Active>) -> Result<()> {
-        let (s, r) = crossbeam_channel::unbounded();
+    fn start_pcap(&mut self) -> Result<crossbeam_channel::Receiver<Event>> {
+        let mut capture = pcap::Capture::from_device(self.interface_name.as_str())?
+            .promisc(true)
+            .open()?;
+        capture.direction(pcap::Direction::In)?;
+        capture.filter("arp or (udp and port bootpc)")?;
 
+        let (s, r) = crossbeam_channel::unbounded();
         std::thread::spawn(move || loop {
             match capture.next() {
                 Ok(packet) => {
@@ -80,52 +97,115 @@ impl HouseRat {
             };
         });
 
+        Ok(r)
+    }
+
+    fn run(&mut self) -> Result<()> {
+        let cap_r = self.start_pcap()?;
+
+        let (resolve_s, resolve_r) = crossbeam_channel::unbounded();
+        let resolver = Resolver::new().expect("Failed to create resolver");
+        for device in self.devices.as_ref().unwrap() {
+            let resolve_s2 = resolve_s.clone();
+            let mac = device.mac;
+            resolver.query_a(&device.hostname, move |result| match result {
+                Ok(result) => {
+                    for a_result in result.into_iter() {
+                        if let Err(e) = resolve_s2.send((mac, a_result.ipv4())) {
+                            println!("Failed to send address resolution: {}", e);
+                        }
+                    }
+                }
+                Err(e) => println!("Failed to resolve: {}", e),
+            });
+        }
+        drop(resolve_s);
+        let mut resolve_r = Some(&resolve_r);
+
         let clock = crossbeam_channel::tick(std::time::Duration::from_secs(TICK_SECS.into()));
 
         #[allow(clippy::drop_copy, clippy::zero_ptr)]
         loop {
             select! {
-                recv(r) -> event => match event? {
-                    Event::Connected(mac) => {
-                        if self.online.contains_key(&mac) {
-                            println!("Device {} reconnected, skipping notification", mac);
-                        } else {
-                            self.notify(mac, Status::Arrived);
-                        }
+                recv(cap_r) -> event => self.handle_event(event?),
+                recv(clock) -> _ => self.handle_clock(),
+                recv(resolve_r.unwrap_or(&never())) -> device => match device {
+                    Ok((mac, ip)) => self.handle_resolve(mac, ip),
+                    Err(_) => {
+                        resolve_r = None;
+                        self.devices = None;
                     }
-                    Event::Announced { mac, ip } => {
-                        if self.rules.contains_key(&mac) {
-                            let _ = self.online.insert(mac, Tracking { ip, outstanding: 0 });
-                        }
-                    }
-                    Event::Responded(mac) => {
-                        if let Some(tracking) = self.online.get_mut(&mac) {
-                            println!("Got keepalive response from {}", mac);
-                            tracking.outstanding = 0;
-                        }
-                    }
-                    Event::Ignored => (),
                 },
-                recv(clock) -> _ => {
-                    let mut left = Vec::new();
-                    for (mac, tracking) in &mut self.online {
-                        if tracking.outstanding < ALLOWED_PACKETS_LOST {
-                            println!("Sending keepalive to {} ({}), outstanding: {}", tracking.ip, mac, tracking.outstanding);
-                            match self.socket.send_arp_request(&self.network_addresses, &NetworkAddresses::new(*mac, tracking.ip)) {
-                                Ok(()) => tracking.outstanding += 1,
-                                Err(e) => println!("Failed to send keepalive: {}", e),
-                            }
-                        } else {
-                            println!("Assuming {} left after not receiving response for {} seconds", mac, tracking.outstanding * TICK_SECS);
-                            left.push(*mac);
-                        }
-                    }
-                    for mac in left {
-                        let _ = self.online.remove(&mac);
-                        self.notify(mac, Status::Left);
-                    }
+            }
+        }
+    }
+
+    fn handle_resolve(&self, mac: MacAddr, ip: std::net::Ipv4Addr) {
+        println!("Resolved: {}", ip);
+        if let Err(e) = self
+            .socket
+            .send_arp_request(&self.network_addresses, &NetworkAddresses::new(mac, ip))
+        {
+            println!("Failed to send ARP request to {}: {}", ip, e);
+        }
+    }
+
+    fn handle_event(&mut self, event: Event) {
+        match event {
+            Event::Connected(mac) => {
+                if self.online.contains_key(&mac) {
+                    println!("Device {} reconnected, skipping notification", mac);
+                } else {
+                    self.notify(mac, Status::Arrived);
                 }
             }
+            Event::Alive { mac, ip } => {
+                if self.rules.contains_key(&mac) {
+                    println!("Device {} is alive", mac);
+                    match self.online.entry(mac) {
+                        hash_map::Entry::Occupied(mut occupied) => {
+                            occupied.get_mut().outstanding = 0
+                        }
+                        hash_map::Entry::Vacant(vacant) => {
+                            vacant.insert(Tracking { ip, outstanding: 0 });
+                        }
+                    }
+                }
+                if let Some(tracking) = self.online.get_mut(&mac) {
+                    tracking.outstanding = 0;
+                }
+            }
+            Event::Ignored => (),
+        }
+    }
+
+    fn handle_clock(&mut self) {
+        let mut left = Vec::new();
+        for (mac, tracking) in &mut self.online {
+            if tracking.outstanding < ALLOWED_PACKETS_LOST {
+                println!(
+                    "Sending keepalive to {} ({}), outstanding: {}",
+                    tracking.ip, mac, tracking.outstanding
+                );
+                match self.socket.send_arp_request(
+                    &self.network_addresses,
+                    &NetworkAddresses::new(*mac, tracking.ip),
+                ) {
+                    Ok(()) => tracking.outstanding += 1,
+                    Err(e) => println!("Failed to send keepalive: {}", e),
+                }
+            } else {
+                println!(
+                    "Assuming {} left after not receiving response for {} seconds",
+                    mac,
+                    tracking.outstanding * TICK_SECS
+                );
+                left.push(*mac);
+            }
+        }
+        for mac in left {
+            let _ = self.online.remove(&mac);
+            self.notify(mac, Status::Left);
         }
     }
 
@@ -179,16 +259,9 @@ fn run() -> Result<()> {
     let config = config::Config::from_file(opt.config_file)?;
 
     println!("Listening on interface {}...", config.interface.name);
-    let device = pcap::Device {
-        name: config.interface.name.clone(),
-        desc: None,
-    };
-    let mut capture = pcap::Capture::from_device(device)?.promisc(true).open()?;
-    capture.direction(pcap::Direction::In)?;
-    capture.filter("arp or (udp and port bootpc)")?;
 
     let mut houserat = HouseRat::new(config)?;
-    houserat.run(capture)
+    houserat.run()
 }
 
 fn main() {
